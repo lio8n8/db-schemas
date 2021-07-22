63c8bf6ac7c1f27606c155f50ad9069f8667172c
lio8n8 <lio8n8@gmail.com>
2021-07-22 21:41:13 +0300

    Updated theme component

--- index.js
+++ index.js
@@ -77,52 +77,6 @@ saveAsSvgBtnEl.addEventListener('click', () => {
     document.body.removeChild(downloadLink);
 });
 
-function renderThemes(container) {
-    /*
-    <div id="themes-list" class="themes-list">
-                <div class="theme">
-                    <span>test0</span>
-                    <div class="theme-colors">
-                        <div class="theme-color"></div>
-                        <div class="theme-color"></div>
-                        <div class="theme-color"></div>
-                        <div class="theme-color"></div>
-                        <div class="theme-color"></div>
-                    </div>
-                </div>
-            </div>
-    */
-    defaultThemes.forEach(theme => {
-        const themeEl = document.createElement('div');
-        themeEl.className = 'theme';
-        const themeNameEl = document.createElement('span');
-        themeNameEl.textContent = theme.name;
-
-        const themeColorsEl = document.createElement('div');
-        themeColorsEl.className = 'theme-colors';
-
-        // Colors.
-        const backgroundColor = document.createElement('div');
-        backgroundColor.className = 'theme-color';
-        backgroundColor.style.backgroundColor = theme.backgroundColor;
-        themeColorsEl.appendChild(backgroundColor);
-
-        const borderColor = document.createElement('div');
-        borderColor.className = 'theme-color';
-        borderColor.style.backgroundColor = theme.borderColor;
-        themeColorsEl.appendChild(borderColor);
-
-
-        themeEl.appendChild(themeNameEl);
-        themeEl.appendChild(themeColorsEl);
-        container.appendChild(themeEl);
-    });
-}
-
-const themesListEl = document.getElementById('themes-list');
-renderThemes(themesListEl);
-
-
 function validateData(data) {
     return true;
 }
--- js/Theme.js
+++ js/Theme.js
@@ -4,6 +4,42 @@ export class Theme {
     constructor() {
         this.themes = defaultThemes;
         this.currentTheme = defaultThemes[0];
+        this.themeListContainerEl = document.getElementById('themes-list');
+
+        this.init();
+    }
+
+    init() {
+
+        defaultThemes.forEach(theme => {
+            const themeEl = document.createElement('div');
+            themeEl.className = 'theme';
+            themeEl.id = theme.name;
+            const themeNameEl = document.createElement('span');
+            themeNameEl.textContent = theme.name;
+
+            const themeColorsEl = document.createElement('div');
+            themeColorsEl.className = 'theme-colors';
+
+            // Colors.
+            const backgroundColor = document.createElement('div');
+            backgroundColor.className = 'theme-color';
+            backgroundColor.style.backgroundColor = theme.backgroundColor;
+            themeColorsEl.appendChild(backgroundColor);
+
+            const borderColor = document.createElement('div');
+            borderColor.className = 'theme-color';
+            borderColor.style.backgroundColor = theme.borderColor;
+            themeColorsEl.appendChild(borderColor);
+
+
+            themeEl.appendChild(themeNameEl);
+            themeEl.appendChild(themeColorsEl);
+            this.themeListContainerEl.appendChild(themeEl);
+        });
+
+        let defaultTheme = document.getElementById(defaultThemes[0].name);
+        defaultTheme.classList.add('active');
     }
 
     getCurrentTheme() {
