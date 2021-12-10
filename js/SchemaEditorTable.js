feb5dfaa7273fd9ec20b2254466b14ec39184740
lio8n8 <lio8n8@gmail.com>
2021-12-10 23:26:44 +0200

    Render tables in editor

---
+++ js/SchemaEditorTable.js
@@ -0,0 +1,40 @@
+export class SchemaEditorTable {
+    constructor(entityData) {
+        this.schemaEditorEl = document.getElementById('schema-editor');
+        this.entityData = entityData;
+    }
+
+    generateTableDataElAndAppend() {
+        const tableDataEl = document.createElement('div');
+        tableDataEl.className = "table-data";
+
+        const tableDataTitle = document.createElement('div');
+        tableDataTitle.className = 'table-data-row table-data-title';
+        tableDataTitle.textContent = this.entityData.name;
+
+
+        tableDataEl.appendChild(tableDataTitle);
+
+        this.entityData.rows.forEach(row => {
+            const tableDataRowEl = document.createElement('div');
+            tableDataRowEl.className = "table-data-row";
+
+            const tableDataNameEl = document.createElement('input');
+            tableDataNameEl.className = "table-data-name";
+            tableDataNameEl.value = row.name;
+
+            const tableDataTypeEl = document.createElement('input');
+            tableDataTypeEl.className = "table-data-type";
+            tableDataTypeEl.value = row.type;
+
+            tableDataRowEl.appendChild(tableDataNameEl);
+            tableDataRowEl.appendChild(tableDataTypeEl);
+
+            tableDataEl.appendChild(tableDataRowEl);
+        });
+
+        this.schemaEditorEl.appendChild(tableDataEl);
+
+        return tableDataEl;
+    }
+}
